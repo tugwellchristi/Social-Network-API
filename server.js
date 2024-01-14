b6e3486ac569c4d2f1d5a3998e2eb5f5cdfcb7bf
tugwellchristi <tugwellchristi@gmail.com>
2024-01-14 14:23:19 -0800

    removing code for mongoDB, swithching to mongoose in server.js and conn.js

--- server.js
+++ server.js
@@ -1,39 +1,54 @@
 const express = require('express');
-const { MongoClient} = require('mongodb');
+const db = require('./config/connection');
+
+const { User, Thought } = require('./models');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-const connectionStringURI = `mongodb://127.0.0.1:27017`;
-const client = new MongoClient(connectionStringURI);
-
-// Create variable to hold db connection
-let db;
-
-const dbName = 'socialNetworkDB';
-
-// Connecting to MongoDB server
-client.connect()
-// on success...
-.then(() => {
-    console.log('Connected successfully to MongoDB');
-    // Connect to socialNetworkDB
-    db = client.db(dbName);
-
-    // ...start up Express server
-    app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    });
-})
-.catch((err) => {
-    console.error('Mongo connection error: ', err.message);
-});
-
-// Route handlers
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// Post route
-app.post
+app.get('/', async (req, res) => {
 
-// Get route
-app.get
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
+
+// const connectionStringURI = `mongodb://127.0.0.1:27017`;
+// const client = new MongoClient(connectionStringURI);
+
+// // Create variable to hold db connection
+// let db;
+
+// const dbName = 'socialNetworkDB';
+
+// // Connecting to MongoDB server
+// client.connect()
+// // on success...
+// .then(() => {
+//     console.log('Connected successfully to MongoDB');
+//     // Connect to socialNetworkDB
+//     db = client.db(dbName);
+
+//     // ...start up Express server
+//     app.listen(port, () => {
+//         console.log(`Example app listening at http://localhost:${port}`);
+//     });
+// })
+// .catch((err) => {
+//     console.error('Mongo connection error: ', err.message);
+// });
+
+// // Route handlers
+// app.use(express.json());
+
+// // Post route
+// app.post
+
+// // Get route
+// app.get
