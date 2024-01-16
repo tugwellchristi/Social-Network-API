8ff5f2f6d5142ec56775f459f0908074e181d6b9
tugwellchristi <tugwellchristi@gmail.com>
2024-01-16 07:08:43 -0800

    minor changes, trying to resolve thoughtId and friendId not displaying

--- utils/seed.js
+++ utils/seed.js
@@ -1,7 +1,7 @@
 const connection = require('../config/connection');
 const { userData, thoughtData, reactionData } = require('./data');
 const { User, Thought, Reaction } = require('../models');
-// const thoughtController = require('../controllers/thoughtController');
+
 
 connection.on('error', (error) => console.error('MongoDB connection error:', error));
 
@@ -17,19 +17,28 @@ connection.once('open', async () => {
         // Seed users
         const users = await User.insertMany(userData);
 
-        thoughtData.forEach((thought, index) => {
-            thought.userId = users[index]._id;
-        });
-
         // Seed thoughts
         const thoughts = await Thought.insertMany(thoughtData);
 
+        // Populate thoughtId for reactions
         reactionData.forEach((reaction, index) => {
             reaction.thoughtId = thoughts[index]._id;
         });
 
         // Seed reactions
-        await Reaction.insertMany(reactionData);
+        const reactions = await Reaction.insertMany(reactionData);
+
+        // Populate friendId for users
+        users.forEach(async (user) => {
+            const friendIndex = (users.indexOf(user) + 1) % users.length;
+            const friend = users[friendIndex];
+
+            // Add friend to user's friend list
+            await User.findByIdAndUpdate(user._id, { $addToSet: { friends: friend._id } });
+
+            // Add user to friend's friend list
+            await User.findByIdAndUpdate(friend._id, { $addToSet: { friends: user._id } });
+        });
 
         console.log('Data seeded successfully');
     } catch (error) {
