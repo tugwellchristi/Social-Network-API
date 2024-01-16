2fce71f6621f665df4a1ac3e852bdb9b11299bb4
tugwellchristi <tugwellchristi@gmail.com>
2024-01-15 17:37:39 -0800

    updated file structure

--- server.js
+++ server.js
@@ -19,36 +19,3 @@ db.once('open', () => {
     });
 });
 
-// const connectionStringURI = `mongodb://127.0.0.1:27017`;
-// const client = new MongoClient(connectionStringURI);
-
-// // Create variable to hold db connection
-// let db;
-
-// const dbName = 'socialNetworkDB';
-
-// // Connecting to MongoDB server
-// client.connect()
-// // on success...
-// .then(() => {
-//     console.log('Connected successfully to MongoDB');
-//     // Connect to socialNetworkDB
-//     db = client.db(dbName);
-
-//     // ...start up Express server
-//     app.listen(port, () => {
-//         console.log(`Example app listening at http://localhost:${port}`);
-//     });
-// })
-// .catch((err) => {
-//     console.error('Mongo connection error: ', err.message);
-// });
-
-// // Route handlers
-// app.use(express.json());
-
-// // Post route
-// app.post
-
-// // Get route
-// app.get
